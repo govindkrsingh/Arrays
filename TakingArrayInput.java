ec77b9dd6310120cb2e82f3ea41b6b2247c5aaf3
govind singh <govindsingh9771@gmail.com>
2019-01-19 07:50:32 -0800

    Add files via upload
    This example shows how to take input in array from user

---
+++ TakingArrayInput.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class TakingArrayInput {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+
+    System.out.println("enter number of elements");
+
+    int n=s.nextInt();
+
+    int arr[]=new int[n];
+
+    System.out.println("enter elements");
+
+    for(int i=0;i<n;i++){//for reading array
+        arr[i]=s.nextInt();
+
+    }
+
+    for(int i: arr){ //for printing array
+
+        System.out.println(i);
+
+    }
+}
+
+}
